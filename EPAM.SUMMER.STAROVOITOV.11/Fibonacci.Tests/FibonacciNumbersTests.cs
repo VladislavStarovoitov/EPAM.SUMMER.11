c2c8200f24c56c4b484255bd801967c3d3af9b4f
VladislavStarovoitov <VladislavStarovoitov>
2016-07-20 18:29:48 +0300

    Unit tests

---
+++ EPAM.SUMMER.STAROVOITOV.11/Fibonacci.Tests/FibonacciNumbersTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Fibonacci.FibonacciNumbers;
+
+namespace Fibonacci.Tests
+{
+    [TestFixture]
+    public class FibonacciNumbersTests
+    {
+        [TestCase(6, new int[6] { 0, 1, 1, 2, 3, 5})]
+        public void GetNumbersTest(int count, IEnumerable<int> expected)
+        {
+            var result = GetNumbers(count);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestCase(-1, new int[6] { 0, 1, 1, 2, 3, 5 })]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ArgumentOutOfRangeExceptionExpected(int count, IEnumerable<int> expected)
+        {
+            var result = GetNumbers(count);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
