09571f1a026f09c23ae8e91e9a92a6cb7e5bef72
VladislavStarovoitov <VladislavStarovoitov>
2016-07-24 19:25:00 +0300

    BinaryTree

---
+++ EPAM.SUMMER.STAROVOITOV.11/Tree/BinarySearchTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public sealed class BinarySearchTree<T>
+    {
+        private IComparer<T> _comparer;
+        private TreeNode _root;
+
+        public IComparer<T> Comprer
+        {
+            get
+            {
+                return _comparer;
+            }
+            set
+            {
+                if (ReferenceEquals(value, null))
+                    _comparer = Comparer<T>.Default;
+                else
+                    _comparer = value;
+            }
+        }
+
+        private sealed class TreeNode
+        {
+            public TreeNode left { get; set; }
+            public TreeNode right { get; set; }
+            public T Item { get; }            
+
+            TreeNode(T item)
+            {
+                Item = item;
+            }
+        }  
+    }
+}
