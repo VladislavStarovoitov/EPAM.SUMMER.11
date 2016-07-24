cb2f89c1284a15b8426d0d425bfe48b790ec2140
VladislavStarovoitov <VladislavStarovoitov>
2016-07-25 02:23:47 +0300

    Fix add method

--- EPAM.SUMMER.STAROVOITOV.11/Tree/BinarySearchTree.cs
+++ EPAM.SUMMER.STAROVOITOV.11/Tree/BinarySearchTree.cs
@@ -35,7 +35,7 @@ namespace Tree
         {
             foreach (var item in items)
             {
-                Add(item, _root);
+                Add(item);
             }
         }
 
@@ -43,29 +43,84 @@ namespace Tree
         {
             if (ReferenceEquals(item, null))
                 throw new ArgumentNullException();
-            Add(item, _root);
+
+            if (ReferenceEquals(_root, null))
+            {
+                _root = new TreeNode(item, null);
+            }
+            else
+            {
+                Add(item, _root, null);
+            }
         }
 
-        private void Add(T item, TreeNode node)
+        public bool Remove(T item)
+        {
+            if (ReferenceEquals(item, null))
+                throw new ArgumentNullException();
+
+            return Remove(item, _root);
+        }
+
+        private bool Remove(T item, TreeNode node)
+        {
+            if (!ReferenceEquals(node, null))
+                return false;
+
+            if (_comparer.Compare(item, node.Item) > 0) 
+                return Remove(item, node.Right);
+            if (_comparer.Compare(item, node.Item) < 0)
+                return Remove(item, node.Left);
+
+            //if (_comparer.Compare(item, node.Parent.Item) > 0)
+            //    if (ReferenceEquals(node.)) todo
+            //    node.Parent = node.Left;
+            //else
+            //{
+            //    if (ReferenceEquals(node.Left, null))
+            //        node.Parent = node.Right;
+            //    else
+            //    {
+            //        TreeNode rightestNode = node.Left;
+            //        while (ReferenceEquals(rightestNode, null))
+            //            rightestNode = node.Right;
+            //        rightestNode.Parent = rightestNode.Left;
+            //        rightestNode.Left = node.Left;
+            //        node.Parent = rightestNode;
+            //    }
+            //}
+            return true; 
+        }
+
+        private void Add(T item, TreeNode node, TreeNode parent)
         {
             if (ReferenceEquals(node, null))
-                node = new TreeNode(item);
+            {
+                var newNode = new TreeNode(item, parent);
+                if (_comparer.Compare(item, node.Item) > 0)
+                    parent.Right = newNode;
+                if (_comparer.Compare(item, node.Item) < 0)
+                    parent.Left = newNode;
+            }
             else
                 if (_comparer.Compare(item, node.Item) > 0)
-                    Add(item, node.Right);
+                    Add(item, node.Right, node);
                 else
-                    Add(item, node.Left);                    
+                    Add(item, node.Left, node);                    
         }
 
         private sealed class TreeNode
         {
             public TreeNode Left { get; set; }
             public TreeNode Right { get; set; }
+            public TreeNode Parent { get; set; }
+
             public T Item { get; }            
 
-            public TreeNode(T item)
+            public TreeNode(T item, TreeNode parent)
             {
                 Item = item;
+                Parent = parent;
             }
         }  
     }
